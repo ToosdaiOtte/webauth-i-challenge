c4ee0d7132b3aa166f681ac647527bad8fd79ad9
Toosdai Otte <toosdai52Otte@gmail.com>
2019-12-08 16:57:01 -0600

    created users-model file, filled in get request in users-router

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+const Users = require('./users-model.js');
 
+router.get('/', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.send(err)
+        })
 });
 
 module.exports = router;
