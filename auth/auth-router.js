cb9064da54903d8f5ce9d6516880cdf86aef7cbb
Toosdai Otte <toosdai52Otte@gmail.com>
2019-12-08 20:57:14 -0600

    register/login working with auth middleware, mvp complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
+const Users = require('../users/users-model.js')
 
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 router.post('/login', (req, res) => {
--- users/users-router.js
+++ users/users-router.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const authRequired = require('../auth/auth-require-middleware.js');
 
 const Users = require('./users-model.js');
 
-router.get('/', (req, res) => {
+router.get('/', authRequired, (req, res) => {
     Users.find()
         .then(users => {
             res.json(users);
