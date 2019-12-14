2d7f32845695c17b13f4973430c7f201dafd1f63
Toosdai Otte <toosdai52Otte@gmail.com>
2019-12-14 16:00:51 -0600

    sessions and cookies mvp met

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,8 +24,9 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if(user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({ 
-                    message: `Welcome ${user.username}!`
+                    message: `Welcome ${user.username}! have a cookie...`
                 });
             } else {
                 res.status(401).json({
@@ -36,6 +37,20 @@ router.post('/login', (req, res) => {
         .catch(err => {
             res.status(500).json(err)
         })
+});
+
+router.delete('/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy(err => {
+            if(err) {
+                res.status(400).send('unable to logout...')
+            } else {
+                res.send('Adios!')
+            }
+        });
+    } else {
+        res.end();
+    }
 });
 
 module.exports = router;
